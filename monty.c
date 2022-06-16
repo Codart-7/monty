68e38c0bd247a73dad0c8df9d567edf286af48f9
Michael Adisa <57605457+Codart-7@users.noreply.github.com>
2022-06-16 08:20:03 +0100

    Update monty.c

--- monty.c
+++ monty.c
@@ -3,9 +3,9 @@
 glob_t glob;
 
 /**
- * stack_init - initialize the stack
- * @head: top of the stack
- */
+ * stack_init - initialize all the things
+ * @head: top of stack data structure
+ **/
 void stack_init(stack_t **head)
 {
 	*head = NULL;
@@ -14,13 +14,15 @@ void stack_init(stack_t **head)
 
 /**
  * free_all - free all malloc'ed memory
- * This is available "atexit", starting at getline loop
- */
+ *     note: this is available "atexit", starting at
+ *           getline loop
+ **/
 void free_all(void)
 {
 	stack_t *tmp1, *tmp2 = NULL;
 
 	tmp1 = *(glob.top);
+	/* printf("glob.top->%p\n",  (void*)glob.top); */
 	while (tmp1 != NULL)
 	{
 		tmp2 = tmp1->next;
@@ -30,12 +32,11 @@ void free_all(void)
 }
 
 /**
- * main - Entry point
+ * main - monty bytecode interpreter
  * @argc: number of command line arguments
- * @argv: array of strings containing the command line arguments
- *
- * Return: EXIT_SUSSESS or EXIT_FAILURE
- */
+ * @argv: array of strings containing the comm line args
+ * Return: EXIT_SUCCESS or EXIT_FAILURE!!!
+ **/
 int main(int argc, char **argv)
 {
 	stack_t *head;
@@ -43,7 +44,7 @@ int main(int argc, char **argv)
 	stack_init(&head);
 	if (argc != 2)
 	{
-		dprintf(2, "USAGE: monty file\n");
+		printf("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	process_file(argv[1], &head);
