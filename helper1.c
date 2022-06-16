6ff9a85c372296d7c0722af99835616927a21899
Michael Adisa <57605457+Codart-7@users.noreply.github.com>
2022-06-16 08:22:23 +0100

    Update helper1.c

--- helper1.c
+++ helper1.c
@@ -2,10 +2,10 @@
 
 /**
  * process_file - process the whole monty file
- * @filename: string name of the file
- * @stack: double pointer o the top of the stsck
+ * @filename: str name of monty opcode file
+ * @stack: double pointer to top of stack data struct
  * Return: return an error code or success
- */
+ **/
 int process_file(char *filename, stack_t **stack)
 {
 	size_t len;
@@ -15,29 +15,26 @@ int process_file(char *filename, stack_t **stack)
 	FILE *fp;
 	char *op;
 
+
 	if (!filename)
 	{
-		dprintf(2, "Error: Can't open file %s\n", filename);
+		printf("Error: Can't open file %s\n", filename);
 		exit(EXIT_FAILURE);
 	}
-
 	fp = fopen(filename, "r");
 	if (fp == NULL)
 	{
-		dprintf(2, "Error: Can't open file %s\n", filename);
+		printf("Error: Can't open file %s\n", filename);
 		exit(EXIT_FAILURE);
 	}
-
 	atexit(free_all);
-
-	while ((read = getline(&line, &len, fp)) != 1)
+	while ((read = getline(&line, &len, fp)) != -1)
 	{
 		op = strtok(line, DELIMS);
 		line_number++;
 		if (op)
 			delegate_op(stack, op, line_number);
 	}
-
 	free(line);
 	fclose(fp);
 	return (EXIT_SUCCESS);
